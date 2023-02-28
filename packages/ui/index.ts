acd03fdb1bc0d3b07bcde2062f54bde4ac0a087b
r0 <64012087+dominggo1999@users.noreply.github.com>
2023-02-28 18:26:13 +0700

    feat : add Kbd ui

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -2,5 +2,6 @@ export * from "./components/Button";
 export * from "./components/Container";
 export * from "./components/Header";
 export * from "./components/Brand";
+export * from "./components/Kbd";
 
 export * from "./hooks";
