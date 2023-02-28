9ee88cb2fcf7e7375f1492e0591fd6a5cb8eceb5
r0 <64012087+dominggo1999@users.noreply.github.com>
2023-03-01 01:38:21 +0700

    feat : add contact and about page

--- apps/commerze/src/pages/About.tsx
+++ apps/commerze/src/pages/About.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import { Container } from "@acme/ui";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <Container className="mt-5 flex flex-col items-center text-center">
+      <h1 className="mb-4 text-3xl font-bold text-primary">About Us</h1>
+      <p className="mb-8 max-w-[700px] text-gray-500 dark:text-white sm:text-lg xl:w-1/2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor
+        neque eu convallis mollis. Proin finibus tristique libero, sed blandit
+        nulla convallis vel. Donec euismod consectetur ligula, sed bibendum est
+        gravida id.
+      </p>
+    </Container>
+  );
 };
 
 export default About;
--- apps/commerze/src/pages/Contact.tsx
+++ apps/commerze/src/pages/Contact.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { FaEnvelope, FaMapMarkerAlt, FaPhone } from "react-icons/fa";
+import { Container } from "@acme/ui";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <Container className="mt-6">
+      <div className="flex flex-col items-center">
+        <h1 className="mb-8 text-3xl font-bold text-primary">Contact Us</h1>
+
+        <div className="mb-6 flex items-center space-x-4">
+          <FaMapMarkerAlt className="text-2xl text-primary" />
+          <p className="text-lg">1234 Main Street, Anytown USA</p>
+        </div>
+
+        <div className="mb-6 flex items-center space-x-4">
+          <FaPhone className="text-2xl text-primary" />
+          <p className="text-lg">(123) 456-7890</p>
+        </div>
+
+        <div className="flex items-center space-x-4">
+          <FaEnvelope className="text-2xl text-primary" />
+          <Link
+            target="_blank"
+            to="mailto:info@test.com"
+            className="text-lg hover:text-primary"
+          >
+            info@acme.com
+          </Link>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Contact;
